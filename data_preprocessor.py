3682c170e26c797977a461e468d8aae7e04fe356
Sean Rountree <srountre@termpail.umd.edu>
2021-09-17 13:29:18 -0400

    Untested loading of data into json files

--- data_preprocessor.py
+++ data_preprocessor.py
@@ -1,1 +1,27 @@
 # data preprocessor 
+
+from torch.utils.data import DataLoader
+import json
+
+def process_data(training_data, testing_data, train_file, test_file):
+    train_dataloader = DataLoader(training_data, batch_size = 64, shuffle = True)
+    test_dataloader = DataLoader(testing_data, batch_size = 64, shuffle = True)
+
+    train_dict = get_dict(train_dataloader)
+    test_dict = get_dict(test_dataloader)
+
+    with open(train_file, "w") as outfile:
+        json.dump(train_dict, outfile)
+    
+    with open(test_file, "w") as outfile:
+        json.dump(test_dict, outfile)
+
+def get_dict(dataloader):
+    toReturn = {}
+    for i in dataloader:
+        features, lables = i
+        img = features[0].squeeze()
+        label = label[0]
+        toReturn[img] = label
+    return toReturn
+        
