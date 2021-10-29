fc7899ad1f2691e09961b0a07d7fa86e05c2c88d
Jesuseyi Oyeyemi <joyeyemi@terpmail.umd.edu>
2021-10-29 14:31:43 -0400

    Cleaned up the branch

--- src/data_preprocessor.py
+++ src/data_preprocessor.py
@@ -1,57 +1,95 @@
 # data preprocessor 
-
-from torch.utils.data import DataLoader
-from torch.utils.data import Dataset
-
+import torch
+import torch.utils.data as data
+from torchvision import transforms
 from PIL import Image
 import json
 import os
-import pandas as pd
-from torchvision.io import read_image
+import random
 
-class Kitti(Dataset):
-    def __init__(self, img_dir, depth_maps, transform = None, target_transform = None):
+
+class Kitti(data.Dataset):
+    def __init__(self, img_dir, depth_maps):
         self.depth_maps = depth_maps
         self.img_dir = img_dir
-        self.transform = transform
-        self.target_transform = target_transform
+        process_data(depth_maps, img_dir, 'Kitti.json')
+
     def __len__(self):
         totalFiles = 0
-
         for base, dirs, files in os.walk(self.depth_maps):
             for Files in files:
                 totalFiles += 1
-        return totalFiles - 1
+
+        return totalFiles
     
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, str(idx) + '.png')
-        depth_dir = os.path.join(self.depth_maps, str(idx) + '.png')
-        #image = read_image(img_path)
+
+        data = json.load(open('Kitti.json'))
+
+
+        img_path = data[str(idx)][1]
+        depth_dir = data[str(idx)][0]
+      
         image = Image.open(img_path)
         label = Image.open(depth_dir)
-      
+
+        image_width, image_height = image.size
+        
+
+        x = random.randint(0, image_width - 200)
+        y = random.randint(0, image_height - 200)
+
+        image = image.crop((x, y, x + 200, y + 200))
+        label = label.crop((x, y, x + 200, y + 200))
         return image, label
-       
 
-def process_data(training_data, testing_data, train_file, test_file):
-    train_dataloader = DataLoader(training_data, batch_size = 64, shuffle = True)
-    test_dataloader = DataLoader(testing_data, batch_size = 64, shuffle = True)
+def preprocessing(batch_size, is_img_aug=True):
 
-    train_dict = get_dict(train_dataloader)
-    test_dict = get_dict(test_dataloader)
+    train_set = Kitti('dataset/images, dataset/depthmaps')
+    train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    val_set = Kitti('dataset/images, dataset/depthmaps')
+    val_dataloader = torch.utils.data.DataLoader(val_set, batch_size=batch_size, shuffle=False)
+    test_set = Kitti('dataset/images, dataset/depthmaps')
+    test_dataloader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False)
+    all_set = Kitti('dataset/images, dataset/depthmaps')
+    all_dataloader = torch.utils.data.DataLoader(all_set, batch_size=batch_size, shuffle=True)
 
-    with open(train_file, "w") as outfile:
-        json.dump(train_dict, outfile)
-    
-    with open(test_file, "w") as outfile:
-        json.dump(test_dict, outfile)
+    return train_dataloader, val_dataloader, test_dataloader, all_dataloader
 
-def get_dict(dataloader):
-    toReturn = {}
-    for i in dataloader:
-        features, labels = i
-        img = features[0].squeeze()
-        label = labels[0]
-        toReturn[img] = label
+
+
+
+
+
+def get_file_number(filename):
+    toReturn = ""
+    for element in filename:
+        if element != "0":
+            toReturn += element
+
     return toReturn
+    
+def add_zeroes(filename, length):
+    while len(filename) < length:
+        filename = '0' + filename
+    return filename
+
+def get_img_file(filename):
+    return add_zeroes(get_file_number(filename), 10)
+
+    
+
+def process_data(label_path, img_path, file):
+    dict = {}
+    count = 0
+    for filename in os.listdir(label_path):
+        dict[count] = (os.path.join(label_path, filename), os.path.join(img_path, get_img_file(filename)))
+        count += 1
+    
+    with open(file, "w") as outfile:
+        json.dump(dict, outfile)
+    
+
+
+    
         
--- test/test_data_processor.py
+++ test/test_data_processor.py
@@ -1,35 +1,41 @@
-import torch
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
+
+import sys
+import os
 import matplotlib.pyplot as plt
+sys.path.append('src')
+print("\n---------\n" + os.getcwd()+"\n---------\n")
+
 from data_preprocessor import Kitti
 import pytest
 
+
 @pytest.fixture
 
-
 def my_data_generator():
-    dataset = Kitti('images','depth_maps')
+    dataset = Kitti('dataset/images','dataset/depth_maps')
     return dataset
-    
+
 def test_len(my_data_generator):
-    assert my_data_generator.__len__() == 5
+    assert my_data_generator.__len__() == 6
 
 def test_get_item(my_data_generator):
-    my_data_generator.__getitem__(0)
-    assert True == True
+    my_data_generator[0]
+    assert True
 
 def test_display(my_data_generator):
-    img, label = my_data_generator.__getitem__(0)
-    plt.imshow(img)
-    plt.show()
-    plt.imshow(label)
-    plt.show()
+    img, label = my_data_generator[0]
+    # plt.imshow(img)
+    # plt.show()
+    # plt.imshow(label)
+    # plt.show()
+    assert True
+
+
+
+
+
     
 
-    assert True == True
 
 
 
