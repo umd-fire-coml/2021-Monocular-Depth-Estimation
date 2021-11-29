bd098ae0d48ba5ebb6fbc1cba0d76f9c4512eb1a
Siyuan Peng <siyuan.kevin.peng@gmail.com>
2021-11-29 18:21:13 -0500

    fix model summary

--- src/model_summary.py
+++ src/model_summary.py
@@ -1,6 +1,5 @@
 from UNet_model import *
 from torchsummary import summary
 
-inputs = torch.randn((2, 3, 256, 256))
 model = build_unet()
-summary(model, (2, 3, 256, 256))
+summary(model, (3, 256, 256))
